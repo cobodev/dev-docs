bd85f7d7d05a01dfd97cae5c5463460309ef4d10
Jose Luis Cobo <>
2025-10-25 19:01:25 +0200

    Separate index in init and create commands

--- src/create/index.ts
+++ src/create/index.ts
@@ -1,13 +1,44 @@
 import { Command } from "commander";
+import { addDocLinkToIndex, copyFile, getModules, getParametersFromFile, pathExists, replaceInFile } from "../utils";
+import { askParameters, chooseModule } from "../prompts";
+import { error } from "../messages";
+
+const CURRENT_PATH = './'
+
+const exit = async () => {
+  error('Project setup cancelled.')
+  process.exit(0);
+}
 
 export const createCommand = new Command("create")
   .argument("[module]", "El módulo donde se creará la documentación")
   .argument("[title]", "El título de la documentación")
   .description("Crea un nuevo módulo de documentación")
-  .action((module?: string, title?: string) => {
-    if (!module || !title) {
-      console.log("En este caso se pregunta por el modulo y el título");
-    } else {
-      console.log(`Módulo: ${module}, Título: ${title}`);
+  .action(async (module?: string, title?: string) => {
+    // Comprobaciones previas
+    const existIndex = pathExists(`${CURRENT_PATH}/index.md`)
+    const existTemplatesFolder = pathExists(`${CURRENT_PATH}/templates`)
+
+    if (!existIndex && !existTemplatesFolder) {
+      console.log("The directory should be empty")
+      exit()
     }
+
+    // Preguntar que quiere crear (modulos)
+    const modules = getModules(CURRENT_PATH)
+    const selectedModule = await chooseModule(modules)
+  
+    // Resto de preguntas (titulo, ...)
+    const parameters = getParametersFromFile(`${CURRENT_PATH}/templates/${selectedModule}_template.md`)
+    const answers = await askParameters(parameters)
+    console.log(answers)
+
+    const code = '12345'
+    const tit = 'hola adios jaja'
+    // Creación del .md
+    copyFile(`${CURRENT_PATH}/templates/${selectedModule}_template.md`, `${CURRENT_PATH}/${selectedModule}/${code}_${tit.replace(' ', '-')}.md`)
+    // Sustitución de los parámetros
+    replaceInFile(`${CURRENT_PATH}/${selectedModule}/${code}_${tit.replace(' ', '-')}.md`, answers);
+    // Edición del index.md correspondiente
+    addDocLinkToIndex(`${CURRENT_PATH}/index.md`, `${selectedModule}/${code}_${tit.replace(' ', '-')}.md`)
 });
--- src/init/index.ts
+++ src/init/index.ts
@@ -1,7 +1,38 @@
 import { Command } from "commander";
+import { addModuleLink, copyFile, createDirectory, getModules, isEmptyPath, replaceInFile } from "../utils";
+import { askDefaultModules, askProjectName } from "../prompts";
+
+const CURRENT_PATH = './'
+const DEFAULTS_PATH = `${__dirname}/../defaults`
 
 export const initCommand = new Command("init")
   .description("Inicializa la estructura base de documentación")
-  .action(() => {
-    console.log("hola hola adios")
-});
+  .action(async () => {
+    if (isEmptyPath(CURRENT_PATH)) {
+      const projectName = await askProjectName()
+      const modules = getModules(DEFAULTS_PATH)
+      const selectedModules = await askDefaultModules(modules)
+      
+      // Crear un index principal con los links de los modulos elegidos
+      copyFile(`${DEFAULTS_PATH}/index.md`, `${CURRENT_PATH}/index.md`)
+  
+      // Modificar el index con el nombre del proyecto
+      replaceInFile(`${CURRENT_PATH}/index.md`, { 'project name': projectName });
+      
+      // Crear una carpeta templates
+      createDirectory(CURRENT_PATH, 'templates')
+  
+      for (let mod of selectedModules) {
+        // Crear una carpeta por cada modulo elegido
+        createDirectory(CURRENT_PATH, mod)
+        // Mover index dentro de cada modulo
+        copyFile(`${DEFAULTS_PATH}/${mod}/index.md`, `${CURRENT_PATH}/${mod}/index.md`)
+        // Mover los templates dentro de la carpeta templates
+        copyFile(`${DEFAULTS_PATH}/${mod}/template.md`, `${CURRENT_PATH}/templates/${mod}_template.md`)
+        // Añadir al index.md el indice con los modulos
+        addModuleLink(`${CURRENT_PATH}/index.md`, mod);
+      }
+    } else {
+      console.log("The directory should be empty")
+    }
+  });
